732689c42e0e249083a937f8320dea2f4534cd89
git config --global user.name Your <silviairware372@gmail.com>
2024-09-02 16:05:11 +0300

    webpack05

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,11 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-$(document).ready(function() {
-    // Create the paragraph elements
-    const paragraph1 = $('<p>').text('Holberton Dashboard');
-    const paragraph2 = $('<p>').text('Dashboard data for the students');
-    const button = $('<button>').text('Click here to get started');
-    const paragraph3 = $('<p>').attr('id', 'count');
-    const paragraph4 = $('<p>').text('Copyright - Holberton School');
-
-    // Append the elements in the desired order
-    $('body').append(paragraph1, paragraph2, button, paragraph3, paragraph4);
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 let conunt = 0;
 
