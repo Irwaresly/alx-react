a9ddae5c46eb1cb227d83341c287ea96c7aabbdf
git config --global user.name Your <silviairware372@gmail.com>
2024-09-04 13:34:30 +0300

    intro23

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -5,29 +5,20 @@ import { getLatestNotification } from "./utils";
 
 function Notifications() {
   return (
-    <div className="Notifications"> style={{ border: "1px dashed red", padding: "10px", position: "relative" }}
-    <button
-        style={{
-          position: "absolute",
-          right: "10px",
-          top: "10px",
-          background: "transparent",
-          border: "none",
-          cursor: "pointer",
-        }}
+    <div className="Notifications">
+      <button
+        style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "15px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
         aria-label="Close"
-        onClick={() => console.log("Close button has been clicked")}
+        onClick={console.log("Close button has been clicked")}
       >
-        <img src={closeIcon} alt="close icon" />
+        <img src={closeIcon} alt="closeIcon" width="10px" />
       </button>
-
       <p>Here is the list of notifications</p>
-    
-    <ul>
-      <li data-priority="default">New course available</li>
-      <li data-priority="urgent">New resume available</li>
-      <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
-    </ul>
+      <ul>
+        <li data="default">New course available</li>
+        <li data="urgent">New resume available</li>
+        <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
     </div>
   );
 }
