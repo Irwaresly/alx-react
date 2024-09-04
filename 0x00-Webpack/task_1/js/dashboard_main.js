770d67f3f06c8efa9c5e483d165849258a7a0cd1
git config --global user.name Your <silviairware372@gmail.com>
2024-09-04 15:08:48 +0300

    webpack03

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,27 +1,17 @@
-// Import jQuery and Lodash
 import $ from 'jquery';
 import _ from 'lodash';
 
-$(document).ready(function() {
-    // Create the paragraph and button elements
-    const paragraph1 = $('<p>').text('Holberton Dashboard');
-    const paragraph2 = $('<p>').text('Dashboard data for the students');
-    const button = $('<button>').text('Click here to get started');
-    const paragraph3 = $('<p>').attr('id', 'count');
-    const paragraph4 = $('<p>').text('Copyright - Holberton School');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-    // Append the elements to the body
-    $('body').append(paragraph1, paragraph2, button, paragraph3, paragraph4);
+let count = 0;
 
-    // Initialize counter
-    let count = 0;
+function updateCounter() {
+  count++;
+  $("#count").text(`${count} clicks on the button`);
+};
 
-    // Define the updateCounter function
-    function updateCounter() {
-        count++;
-        $('#count').text(`${count} clicks on the button`);
-    }
-
-    // Bind the debounced updateCounter function to the button's click event
-    button.on('click', _.debounce(updateCounter, 300));
-});
+$('button').on('click', _.debounce(updateCounter, 500));
